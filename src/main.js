aa85ccedfdd7109202c3f726a399e5d9f88bbc6a
System Administrator <root@192.168.2.4>
2022-10-17 19:06:03 +0800

    主页导航样式

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import store from './store'
 import router from './router'
 
 import '@/icons' // icon
-// import '@/permission' // permission control
+import '@/permission' // permission control
 
 Vue.use(ElementUI, { locale })
 
